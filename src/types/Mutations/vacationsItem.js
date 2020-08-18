81394296c9d22b16c7485fc0db629aa4344180c5
Ignacio Carlos Monzalvo Milan <ignacio.monzalvo@fing.edu.uy>
2020-08-17 22:56:20 -0300

    Delete bookings on employee vacations

--- src/types/Mutations/vacationsItem.js
+++ src/types/Mutations/vacationsItem.js
@@ -1,4 +1,58 @@
-const CreateVacationsItem = (parent, { from, to, employeeId }, ctx) => {
+const moment = require('moment')
+
+const getDatesRange = (from, to) => {
+  let currentDate = moment(from)
+  const lastDay = moment(to)
+  let res = []
+  while (currentDate.isBefore(lastDay)) {
+    const rawDate = currentDate.format('YYYY-MM-DD')
+    res.push(rawDate)
+    currentDate = currentDate.add(1, 'days')
+    console.log('res', res)
+  }
+  return res
+}
+
+const generateQueryWhereObject = (range) => {
+  return range.map((day) => {
+    return {
+      start: {
+        startsWith: day,
+      },
+    }
+  })
+}
+
+const generateBatchDeleteObject = (bookings) => {
+  return bookings.map((booking) => {
+    return { id: booking.id }
+  })
+}
+const CreateVacationsItem = async (parent, { from, to, employeeId }, ctx) => {
+  const vacationsStart = moment(from)
+  const vacationsEnd = moment(to)
+
+  const vacationsDateRange = getDatesRange(from, to)
+  const queryWhereObject = generateQueryWhereObject(vacationsDateRange)
+  const employeeDayBokings = await ctx.prisma.booking.findMany({
+    where: {
+      employeeId,
+      OR: queryWhereObject,
+    },
+  })
+
+  const bookingsOnVacation = employeeDayBokings.filter((booking) => {
+    return moment(booking.start).isBetween(vacationsStart, vacationsEnd)
+  })
+
+  const batchDeleteObject = generateBatchDeleteObject(bookingsOnVacation)
+
+  await ctx.prisma.booking.deleteMany({
+    where: {
+      OR: batchDeleteObject,
+    },
+  })
+
   const vacationsItem = ctx.prisma.vacationsItem.create({
     data: {
       from,
